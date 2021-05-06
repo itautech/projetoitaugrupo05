e1f98a1c3f90c9541d8189331697613e22a23e7f
Angelo Beck <angelobeck@floripa.com.br>
2021-05-06 10:31:48 -0300

    Adicionado comentários ao javascript
    Comentários explicativos para cada função.

--- contato.js
+++ contato.js
@@ -1,5 +1,13 @@
 
+// Esta variável guarda o ID de um campo do formulário.
+// Isso permite que, ao fechar uma mensagem de aviso (quando o usuário não preenche um campo obrigatório)
+// o foco seja transferido direto para o campo que precisa ser preenchido.
+
 var returnToFieldId = false;
+
+// Esta função percorre a lista de campos obrigatórios e verifica se estão preenchidos.
+// Se algum estiver vazio, oculta o formulário, mostra uma mensagem de aviso indicando o campo com problema.
+// Interrompe o loop e retorna false para evitar o envio do formulário.
 
 function checarCampos() {
 	const fields = ["field_name", "field_email", "field_subject", "field_content"];
@@ -17,6 +25,8 @@ function checarCampos() {
 	return true;
 }
 
+// Cria um objeto onde cada propriedade contém o valor de um dos campos do formulário:
+
 function capturarCampos(){
 	const fields = ["field_to", "field_name", "field_phone", "field_email", "field_subject", "field_content"];
 var message = {};
@@ -28,6 +38,9 @@ var message = {};
 	}
 	return message;
 }
+
+// Substitui caracteres especiais do conteúdo por "sequências de escape".
+// Isso evita problemas no transporte e risco de ataques propositais.
 
 function formatarCampoParaTransporte(input){
 	var buffer = '';
@@ -42,11 +55,16 @@ function formatarCampoParaTransporte(input){
 			buffer += "#n";
 		else if(input.charAt(i) == "\r")
 			null;
+		else if(input.charAt(i) == "\0")
+			null;
 		else
 			buffer += input.charAt(i);
 	}
 	return buffer;
 }
+
+// Envia o formulário através de HTTP Request.
+// Exibe uma mensagem de acordo com a resposta do servidor.
 
 function enviar() {
 	if(!checarCampos())
@@ -75,11 +93,15 @@ function enviar() {
 	request.send(JSON.stringify (message));
 }
 
+// Oculta qualquer mensagem que esteja sendo exibida e exibe o formulário.
+
 function retornar() {
 	document.getElementById("form").hidden = false;
 	document.getElementById("form-msg-ok").hidden = true;
 	document.getElementById("form-msg-fail").hidden = true;
 }
+
+// Oculta a mensagem de aviso, exibe o formulário e transfere o foco para o campo onde ocorreu o problema.
 
 function corrigir() {
 	document.getElementById("form").hidden = false;
